9bbb5acb6fdeb1478d8b75e65ec80cd9189b35d7
ZhangShiao123 <1336314435@qq.com>
2021-02-27 22:24:41 +0800

    足部修改

--- src/view/common/footer.vue
+++ src/view/common/footer.vue
@@ -1,29 +1,29 @@
 <template>
-<div id="footer_box">
-    <div id="footer_content">
-        <div id="footer_content_up">
-            <a href="https://space.bilibili.com/5782326">关于我们</a>
-            <a href="https://space.bilibili.com/5782326">招纳贤士</a>
-            <a href="https://space.bilibili.com/5782326">广告服务</a>
-            <a href="https://space.bilibili.com/5782326">开发助手</a>
-            <a href="https://space.bilibili.com/5782326">关于我们</a>
-            <a href="https://space.bilibili.com/5782326">关于我们</a>
-            <a href="https://space.bilibili.com/5782326">关于我们</a>
-        </div>
-        <div id="footer_content_down">
-            <a href="https://space.bilibili.com/5782326">公安备案号11010502030143</a>
-            <a href="https://space.bilibili.com/5782326">京ICP备19004658号</a>
-            <a href="https://space.bilibili.com/5782326">经营性网站备案信息</a>
-            <a href="https://space.bilibili.com/5782326">北京互联网违法和不良信息举报中心</a>
-            <a href="https://space.bilibili.com/5782326">帅逼华乐</a>
+    <div id="footer_box">
+        <div id="footer_content">
+            <div id="footer_content_up">
+                <a href="https://space.bilibili.com/5782326">关于我们</a>
+                <a href="https://space.bilibili.com/5782326">招纳贤士</a>
+                <a href="https://space.bilibili.com/5782326">广告服务</a>
+                <a href="https://space.bilibili.com/5782326">开发助手</a>
+                <a href="https://space.bilibili.com/5782326">关于我们</a>
+                <a href="https://space.bilibili.com/5782326">关于我们</a>
+                <a href="https://space.bilibili.com/5782326">关于我们</a>
+            </div>
+            <div id="footer_content_down">
+                <a href="https://space.bilibili.com/5782326">公安备案号11010502030143</a>
+                <a href="https://space.bilibili.com/5782326">京ICP备19004658号</a>
+                <a href="https://space.bilibili.com/5782326">经营性网站备案信息</a>
+                <a href="https://space.bilibili.com/5782326">北京互联网违法和不良信息举报中心</a>
+                <a href="https://space.bilibili.com/5782326">帅逼华乐</a>
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
 export default {
-name: "footer"
+    name: "footer"
 }
 </script>
 
@@ -31,6 +31,7 @@ name: "footer"
 #footer_box{
     width: 100%;
     height: 90px;
+    border-top:1px solid #999;
 
     display: flex;
     align-items: center;
@@ -40,7 +41,12 @@ name: "footer"
 
     margin-top: 10px;
 
+}
 
+#footer_box a{
+    color: #999;
+    margin-left: 10px;
+    margin-right: 10px;
 }
 
 #footer_content{
@@ -58,21 +64,17 @@ name: "footer"
 
 }
 
-#footer_content_up a{
-    margin-left: 10px;
-    margin-right: 10px;
-}
+
 #footer_content_down{
     display: flex;
     justify-content:center;
     flex-wrap: wrap;
 
+    margin-top: 10px;
+
     width: 80%;
     margin-left: 10%;
 
 }
-#footer_content_down a{
-    margin-left: 10px;
-    margin-right: 10px;
-}
+
 </style>
